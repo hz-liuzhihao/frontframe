1315e7024982fe50f9b6378660ef898540ae0adc
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-01-05 18:34:21 +0800

    fix: 修改一点配置文件

--- webpackproject/build/webpack.pro.config.js
+++ webpackproject/build/webpack.pro.config.js
@@ -1,9 +1,16 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.base.config');
+const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].[hash:5].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: 'http://www.baidu.com',
+  },
   plugins: [
     new CleanWebpackPlugin(),
   ]

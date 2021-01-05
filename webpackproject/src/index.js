832d85ceb6cfc7004bf512f138938be60e4536fc
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-01-05 16:13:19 +0800

    chore: 框架调整

--- webpackproject/src/index.js
+++ webpackproject/src/index.js
@@ -1,5 +1,9 @@
 import dva from 'dva';
+import * as dd from 'dingtalk-jsapi';
 import './index.css';
+import {
+  getUrlParam
+} from './utils/common';
 
 // 1. Initialize
 const app = dva();
@@ -12,6 +16,19 @@ const app = dva();
 
 // 4. Router
 app.router(require('./router').default);
+
+(async function () {
+  const corpId = getUrlParam('corpId');
+  await dd.runtime.permission.requestAuthCode({
+    corpId,
+    onFail: function (err) {
+      console.log(err);
+    },
+    onSuccess: function (res) {
+      console.log(JSON.stringify(res));
+    }
+  })
+})()
 
 // 5. Start
 app.start('#root');

e7aea17b2bd586a8d6161e6902d26482a3651135
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-04-07 19:13:26 +0800

    feat: webpack5资源管理

--- webpackfivedemo/src/index.js
+++ webpackfivedemo/src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   // lodash 现在通过一个 script 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }

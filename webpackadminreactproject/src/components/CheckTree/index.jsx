fa067138f02115a9258f8d17ce8cb2d10f301e3e
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-06-30 15:23:23 +0800

    feat: 管理框架提交checkTree数结构算法转换

--- webpackadminreactproject/src/components/CheckTree/index.jsx
+++ webpackadminreactproject/src/components/CheckTree/index.jsx
@@ -1,6 +1,79 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Divider, Checkbox, Button } from 'antd';
+import styles from './index.less';
 
 /**
  * 权限树组件
  */
-export default class CheckTree extends PureComponent { }
+export default class CheckTree extends PureComponent {
+
+  static propTypes = {
+    datas: PropTypes.array,
+    checkIds: PropTypes.array
+  }
+
+  static defaultProps = {
+    datas: [],
+    checkIds: [],
+  }
+
+  /**
+   * 解析数据
+   * @param {*} datas 
+   * @param {*} result 
+   */
+  parseData(data, result, deep) {
+    const row = [];
+    row.deep = deep;
+    if (data.children instanceof Array) {
+      const { children, ...other } = data;
+      row.push(other);
+      data.children.reverse().forEach(item => this.parseData(item, result, deep + 1));
+    } else {
+      row.push(data);
+    }
+    // 当深度一致时说明是同一行,压在一起
+    if (result[0] && result[0].deep == row.deep) {
+      result[0].unshift(row[0]);
+    } else {
+      result.unshift(row);
+    }
+  }
+
+  /**
+   * 数据转换
+   * @returns 
+   */
+  transformData() {
+    const { datas = [] } = this.props;
+    return datas.map(item => {
+      const result = [];
+      this.parseData(item, result, 0);
+      return result;
+    });
+  }
+
+  doChange = (item) => {
+
+  }
+
+  render() {
+    const { datas } = this.props;
+    const transformData = this.transformData();
+    return <div className={styles.checkTreeContainer}>
+      <div className={styles.header}>
+        <span>已选中</span>
+        <Button type='link'>全选</Button>
+      </div>
+      {transformData.map((item, index) => {
+        return <div key={index}>
+          <Divider />
+          {item.map((ite, index) => <div key={index} style={{ paddingLeft: `${ite.deep * 20}px` }}>
+            {ite.map(it => <Checkbox key={it.id} onChange={this.doChange}>{it.name}</Checkbox>)}
+          </div>)}
+        </div>
+      })}
+    </div>
+  }
+}
--- webpackadminreactproject/src/pages/RoleEdit/index.jsx
+++ webpackadminreactproject/src/pages/RoleEdit/index.jsx
@@ -3,6 +3,7 @@ import { connect } from "dva";
 import { PAGE_CONFIG } from "../../utils/config";
 import styles from "./index.less";
 import BasePage from "../../bcomponents/BasePage";
+import CheckTree from "../../components/CheckTree";
 
 /**
  * 权限管理页面
@@ -19,7 +20,65 @@ class RoleEdit extends BasePage {
 
   render() {
     return <div className={styles.perContainer}>
-      角色编辑
+      <CheckTree
+        datas={[{
+          name: '测试1',
+          id: 1,
+          children: [
+            {
+              name: '测试2',
+              id: 2,
+              children: [
+                {
+                  name: '测试3',
+                  id: 3,
+                }
+              ]
+            },
+            {
+              name: '测试11',
+              id: 11
+            },
+            {
+              name: '测试12',
+              id: 12
+            }
+          ]
+        }, {
+          name: '测试4',
+          id: 4,
+          children: [
+            {
+              name: '测试5',
+              id: 5,
+              children: [
+                {
+                  name: '测试6',
+                  id: 6,
+                },
+                {
+                  name: '测试10',
+                  id: 10,
+                }
+              ]
+            },
+            {
+              name: '测试7',
+              id: 7,
+              children: [
+                {
+                  name: '测试8',
+                  id: 8,
+                },
+                {
+                  name: '测试9',
+                  id: 9,
+                }
+              ]
+            }
+          ]
+        }]}
+      />
     </div>;
   }
 }

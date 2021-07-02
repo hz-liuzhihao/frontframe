570e41c67a3d3cceb8be2b87b82882d2639ff343
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-07-02 16:52:00 +0800

    feat: 管理框架类目组件提交格式

---
+++ webpackadminreactproject/src/components/CategorySelect/index.jsx
@@ -0,0 +1,27 @@
+import react, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * 类目选择组件
+ */
+export class LevelSelectOne extends PureComponent {
+
+  static propTypes = {
+    title: PropTypes.string,
+    
+  };
+
+  static defaultProps = {
+
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+      <div></div>
+    </div>
+  }
+}

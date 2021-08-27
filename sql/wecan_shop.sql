3bbdd5377b095e6606a0accffd0c25bde390302d
liuzhihao <wb-lzh760330@alibaba-inc.com>
2021-08-27 18:21:23 +0800

    feat: 提交wecan_shop数据表设计

--- sql/wecan_shop.sql
+++ sql/wecan_shop.sql
@@ -36,13 +36,43 @@ create table if exists we_shop(
   nick varchar(20) comment '名称',
   post_limit_free decimal comment '店铺下单价格达到多少免邮,为0时则永不免邮',
   status tinyint comment '状态',
-
 ) default charset=utf8 comment='店铺表';
 
 drop table if exists we_shop_coupon;
 create table if exists we_shop_coupon(
+  id bigint comment '优惠券id',
+  shop_id bigint comment '店铺id',
+  is_by_url tinyint comment '是否通过url才能生效',
+  price_limit decimal comment '价格限额',
   can_overlap tinyint comment '是否可以重复叠加',
 ) default charset=utf8 comment='店铺优惠券';
+
+drop table if exists we_shop_live(
+  id bigint comment '直播id',
+  shop_id bigint comment '店铺id',
+  good_ids varchar(255) comment '关联商品id',
+  live_url bigint comment '直播url',
+  `status` tinyint comment '直播状态',
+  create_date bigint comment '创建时间',
+) default charset=utf8 comment='店铺直播';
+
+drop table if exists we_shop_video(
+  id bigint comment '短视频id',
+  shop_id bigint comment '店铺id',
+  good_ids varchar(255) comment '关联商品id',
+  video_url varchar(255) comment '视频url',
+  `status` tinyint comment '视频状态',
+  create_date bigint comment '创建时间',
+) default charset=utf8 comment='店铺短视频';
+
+drop table if exists we_shop_article(
+  id bigint comment '文章id',
+  shop_id bigint comment '店铺id',
+  content longtext comment '文章内容',
+  good_ids varchar(255) comment '关联商品id',
+  `status` tinyint comment '文章状态',
+  create_date bigint comment '创建时间',
+) default charset=utf8 comment='店铺文章';
 
 drop table if exists we_good;
 create table we_good(
@@ -50,19 +80,34 @@ create table we_good(
   shop_id bigint comment '店铺id',
   `name` varchar(200) '商品名称',
   category_id varchar(40) comment '三级类目id',
-  images varchar(2040) comment '详情页',
+  pic_url varchar(255) comment '商品主图',
+  images varchar(2040) comment '商品图片简介',
+  detail_images varchar(2040) comment '详情页',
+  videos varchar(2040) comment '视频',
   main_sku_value bigint comment '需要展示的skuid商品',
   is_free_post bigint comment '是否免邮',
   post_money decimal comment '邮费',
+  comment_count int comment '评论数量',
   create_date bigint '创建时间',
   update_date bigint '更新时间',
 ) default charset=utf8 comment='商品表';
+
+drop table if exists we_good_comment;
+create table we_good_comment(
+  id bigint comment '评论id',
+  good_id bigint comment '商品id',
+  user_id bigint comment '用户id',
+  content varchar(255) comment '评论内容',
+  create_date bigint comment '评论时间',
+) default charset=utf8 comment='商品评论表';
 
 drop table if exists we_good_coupon;
 create table we_good_coupon(
   id bigint comment '优惠券id',
   shop_id bigint comment '店铺id',
   good_id bigint comment '商品id',
+  is_by_url tinyint comment '是否通过url才能使用',
+  price_limit decimal comment '限额使用,0表示无门槛',
   can_overlap tinyint comment '是否可以重复叠加',
 ) default charset=utf8 comment='商品优惠券表';
 
@@ -73,7 +118,7 @@ create table we_sku_type(
   `name` varchar(30) comment 'sku类型,颜色,尺寸等,如果没有就默认',
   status tinyint comment '状态',
   create_date bigint comment '创建时间',
-) default charset=utf8 comment='商品分类表';
+) default charset=utf8 comment='商品sku类型表';
 
 drop table if exists we_sku_value;
 create table we_sku_value(
@@ -81,12 +126,13 @@ create table we_sku_value(
   `name` varchar(30) comment 'sku类型值,红色,蓝色,粉色,28x28',
   status tinyint comment '状态',
   create_date bigint comment '创建时间',
-) default charset=utf8 comment='商品分类值表';
+) default charset=utf8 comment='商品sku类型值表';
 
 drop table if exists we_good_type;
 create table we_good_type(
   id bigint comment '主键id',
   shop_id bigint comment '店铺id',
+  good_id bigint comment '商品id',
   `name` bigint comment '类型名称',
   create_date bigint comment '创建时间',
   pic_url varchar(255) comment '类型图url地址',
@@ -115,11 +161,36 @@ create table we_order(
   post_id bigint comment '物流id',
   post_type tinyint comment '物流类型',
   price decimal comment '订单价格',
+  post_price decimal comment '物流费用',
   status tinyint comment '订单状态',
+  deleted tinyint comment '是否删除',
   create_date bigint comment '创建日期',
 ) default charset=utf8 comment='商品订单';
 
 drop table if exists we_order_good;
-create table we_order_good() default charset=utf8 comment='商品订单明细';
+create table we_order_good(
+  order_id bigint comment '订单id',
+  good_id bigint comment '商品id',
+  good_count int comment '商品数量',
+  price decimal comment '订单商品价格',
+) default charset=utf8 comment='商品订单明细';
 
+drop table if exists we_collect;
+create table we_collect(
+  user_id bigint comment '用户id',
+  collect_id bigint comment '收藏id',
+  `type` tinyint comment '收藏类型,0商品,1店铺,2,3,4其他'
+) default charset=utf8 comment='用户收藏表';
 
+drop table if exists we_hot_search;
+create table we_hot_search(
+  keyword varchar(20) comment '关键字',
+  `count` int comment '热搜数量',
+  create_date bigint comment '关键字创建日期',
+) default charset=utf8 comment='用户30天内热搜,30后迭代清除';
+
+drop table if exists we_shop_message(
+  message_id bigint comment '消息关联id',
+  `type` tinyint comment '类型,0直接询问,1商品,2订单',
+  create_date bigint comment '创建日期',
+) default charset=utf8 comment='消息表';

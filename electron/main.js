e9d5e00a57a1430dd9ea65b06a3f213d227ce4fe
wb-lzh760330 <wb-lzh760330@alibaba-inc.com>
2022-04-19 11:17:00 +0800

    feat: 实现vscode调试

--- electron/main.js
+++ electron/main.js
@@ -1,34 +1,35 @@
-const { app, BrowserWindow, Menu } = require('electron');
-require('./main_process/dialog');
+const { app, BrowserWindow, Menu } = require("electron");
+require("./main_process/dialog");
 
 let mainWindow = null;
 
 // 当所有窗口被关闭了,退出
-app.on('window-all-closed', function () {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", function () {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
 });
 
 // 完成了初始化并且准备创建浏览器窗口的时候
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+app.on("ready", function () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
 
-    mainWindow.openDevTools();
+  mainWindow.openDevTools();
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 
-    // mainWindow.setProgressBar(0.5);
+  // mainWindow.setProgressBar(0.5);
 });
 
 // https://www.jianshu.com/p/d005213283e3
